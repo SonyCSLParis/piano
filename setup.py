aa7ef0e860ffedf27521c915e32c11c5231140fe
Theis Bazin <theis.bazin@outlook.com>
2020-11-10 17:38:14 +0100

    :wrench: Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='piano',
+      version='0.1',
+      url='https://github.com/SonyCSLParis/piano',
+      author=['Gaëtan Hadjeres'],
+      author_email=['gaetan.hadjeres@sony.com'],
+      license='MIT',
+      packages=['BFT'],
+      zip_safe=False)
